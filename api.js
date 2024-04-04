30e6c4c178cead850b14099c8af2d0ea25d80c87
Roman Chuykov <chuykov95@mail.ru>
2024-04-04 11:28:57 +0300

    fix chrome

--- api.js
+++ api.js
@@ -16,6 +16,7 @@ global.client = new Client({
     remotePath:
       "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
   },
+  puppeteer: { product: "chrome", executablePath: "/usr/bin/chromium-browser" },
 });
 
 global.authed = false;
